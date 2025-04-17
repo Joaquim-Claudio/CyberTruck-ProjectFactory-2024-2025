70b0f439786fe403c0e9f2e9bcc397bcb5fa9d8c
Joaquim Claudio <joaquim.claudio2003@gmail.com>
2025-04-17 20:54:05 +0100

    running initial scripts.

--- database/sql/initial.sql
+++ database/sql/initial.sql
@@ -1,3 +1,35 @@
+-- CREATES
+
+CREATE TABLE Vehicle (
+    id SERIAL PRIMARY KEY,
+    registration TEXT NOT NULL
+);
+
+CREATE TABLE Logs (
+    id SERIAL PRIMARY KEY,
+    description TEXT,
+    type TEXT,
+    vehicle_id INTEGER NOT NULL,
+    FOREIGN KEY (vehicle_id) REFERENCES Vehicle(id) ON DELETE CASCADE
+);
+
+CREATE TABLE Sensor (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    unity TEXT,
+    serial_number TEXT,
+    vehicle_id INTEGER NOT NULL,
+    FOREIGN KEY (vehicle_id) REFERENCES Vehicle(id) ON DELETE CASCADE
+);
+
+CREATE TABLE Metrics (
+    id SERIAL PRIMARY KEY,
+    value TEXT NOT NULL,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    sensor_id INTEGER NOT NULL,
+    FOREIGN KEY (sensor_id) REFERENCES Sensor(id) ON DELETE CASCADE
+);
+
 -- INSERTS
 
 -- TB VEHICLE
